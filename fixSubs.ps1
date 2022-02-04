b11fc0e3d53e11727547f1900b966507e7ecfb7d
Nick Mason <nmason@linkedin.com>
2022-02-04 20:38:34 +0000

    first commit with .ps1 creation and initial notes to get started

---
+++ fixSubs.ps1
@@ -0,0 +1,10 @@
+<#
+.SYNOPSIS 
+    This script allows you to select a movie file you have downloaded subtitles for, and will go through the automated effort of unpacking and matching the filenames, then cleaning up the messs
+.EXAMPLE
+    .\fixSubs.ps1
+.NOTES
+    Author:  kerbero5
+    Date:    4 Feb 2022
+    fixSubs.ps1
+#>
